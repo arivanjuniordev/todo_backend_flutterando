f53375e6370a4d7207416b64ce6656b53f74f603
arivanjuniordev <94536010+arivanjuniordev@users.noreply.github.com>
2024-07-15 13:06:55 -0300

    Initial commit

---
+++ src/main/java/br/com/arivanjuniordev/todo_backend_flutterando/TodoBackendFlutterandoApplication.java
@@ -0,0 +1,13 @@
+package br.com.arivanjuniordev.todo_backend_flutterando;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoBackendFlutterandoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoBackendFlutterandoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/arivanjuniordev/todo_backend_flutterando/TodoBackendFlutterandoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.arivanjuniordev.todo_backend_flutterando;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoBackendFlutterandoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
